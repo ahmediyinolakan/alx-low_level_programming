77517ecaaf1b548303c931e7b93438b7611e3ef4
Ahmed  Iyinolakan <ahmediyinolakan@gmail.com>
2022-09-08 16:17:36 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * * main - Printing
- * *
- * * Return: Always 0 (Successful)
- * */
+ * main - Print
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual
-			puzzle");
+		puts("\"Programming is like building a multilingual
+puzzle");
 	return (0);
 } 
