f0bd1989717b1bb0f9c829e60cb7f072465bf6b4
Ahmed Iyinolakan <110994726+ahmediyinolakan@users.noreply.github.com>
2022-09-15 20:52:27 +0100

    More Function Task 0-10

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul - multiplies two integers.
+* @a: stores integer 1
+* @b: stores integer 2
+* Return: on success 0.
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
